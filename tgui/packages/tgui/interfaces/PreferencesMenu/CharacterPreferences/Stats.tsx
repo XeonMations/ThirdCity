5679d5434e53e44a5df0b097501b614f9ec07931
XeonMations <myvilnius@gmail.com>
2025-08-23 17:35:58 +0300

    I realize now i dont know how to TSX

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/CharacterPreferences/Stats.tsx
@@ -0,0 +1,32 @@
+// THIS IS A DARKPACK UI FILE
+import { Stack } from 'tgui-core/components';
+
+export function StatsPage() {
+    return (
+        <>
+        <Stack vertical fill>
+            <Stack.Item mt={15}>
+            <Stack fill g={1} className="PreferencesMenu__Stats">
+                <Stack.Item>
+                <Stack vertical>
+                    1
+                </Stack>
+                </Stack.Item>
+                <Stack.Item mt={-5.9}>
+                <Stack vertical>
+                    2
+                </Stack>
+                </Stack.Item>
+                <Stack.Item>
+                <Stack vertical>
+                    3
+                </Stack>
+                </Stack.Item>
+            </Stack>
+            </Stack.Item>
+        </Stack>
+        </>
+    );
+}
+
+
